e947e7e917ade988eb7af68d0a601c0d28e62c8b
EmP33 <purwinmarek16@gmail.com>
2022-04-05 11:19:22 +0200

    Create list item and deleted some redundant files

--- src/components/ListElement/ListElement.jsx
+++ src/components/ListElement/ListElement.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import CSSModules from "react-css-modules";
 import styles from "./ListElement.module.scss";
 
-const ListElement = () => {
-  return <div>ListElement</div>;
+const ListElement = ({ title, description }) => {
+  return (
+    <li styleName="quote">
+      <h3>{title}</h3>
+      <h6>{description}</h6>
+    </li>
+  );
 };
 
 export default CSSModules(ListElement, styles);
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -7,9 +7,21 @@ import ListElement from "../../components/ListElement/ListElement";
 const Main = () => {
   return (
     <main styleName="app">
-      <h1 className="main-header">React-Components by Emp33</h1>
+      <h1 className="main-header">React-Projects by Emp33</h1>
       <ul styleName="app__list">
-        <ListElement />
+        <ListElement
+          title="Quote Generator"
+          description="Generate random Quote"
+        />
+        <ListElement
+          title="Infinity Scroll"
+          description="Fetch pictures during scrolling"
+        />
+        <ListElement title="Picture In Picture" description="Detached screen" />
+        <ListElement
+          title="Light and Dark Mode"
+          description="Switch between dark and light mode"
+        />
       </ul>
     </main>
   );
